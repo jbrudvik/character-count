6cae9f67b169a02393ee4684a844a1f36f5e96de
Jeremy Brudvik <jeremy@brudvik.me>
2014-05-10 00:18:51 -0700

    Added browser_action icons and extension icons

--- src/background.js
+++ src/background.js
@@ -1,10 +1,22 @@
-var enabled = false;
+var activeIcons = {
+  path: {
+    '19': 'icons/active-19.png',
+    '38': 'icons/active-38.png'
+  }
+};
+
+var inactiveIcons = {
+  path: {
+    '19': 'icons/inactive-19.png',
+    '38': 'icons/inactive-38.png'
+  }
+};
+
+var active = false;
 
 function toggleIcon() {
-  chrome.browserAction.setIcon({
-    path: enabled ? 'icon-inactive.png' : 'icon-active.png'
-  });
-  enabled = !enabled;
+  chrome.browserAction.setIcon(active ? inactiveIcons : activeIcons);
+  active = !active;
 }
 
 chrome.browserAction.onClicked.addListener(toggleIcon);
