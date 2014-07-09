2292841b67190ef4229dfc8b1e58ca6e763b8b5a
Jeremy Brudvik <jeremy@brudvik.me>
2014-07-09 14:21:46 -0700

    Major refactor and update. See following:
    - Refactored code to decouple listening, selection-counting logic, and
      output view.
    - Separated extension state, including browser action icon and title,
      to each tab. Extension state propagates per-tab across page loads.
    - Fixed numerous bugs around actual selection tracking. This is tricky,
      and will likely never be perfect (until browsers supply selection
      change events natively).
    - Changed selection counting to collapse groups of linebreak whitespace
      to one character.
    - Stopped counting newline character at end of line until it is
      actually selected.

--- src/background.js
+++ src/background.js
@@ -1,41 +1,155 @@
-var manifest = chrome.runtime.getManifest();
+/*
+ * Character Count background script
+ */
 
-var inactiveIcons = manifest.browser_action.default_icon;
-var activeIcons = {
-  '19': 'icons/active-19.png',
-  '38': 'icons/active-38.png'
+
+/*
+ * An object that keeps track of extension state, sets browser action UI, and
+ * communicates with underlying tab content scripts.
+ *
+ * Abstracts away per-tab state from invoking code. While all public methods
+ * can take a tabId as a method parameter, it is not required. Without the
+ * tabId parameter, the methods implicitly assume the current tab.
+ *
+ * Extension state is maintained separately for each browser tab. The browser
+ * action button reflects (and controls) the state of only the current tab.
+ *
+ * On page loads, the tab will keep its state (reloading the tab in content
+ * script if necessary).
+ */
+function CharacterCount() {
+  // Icons and titles (true -> active, false -> inactive)
+  var manifest = chrome.runtime.getManifest();
+  this.icons = {
+    true: {
+      '19': 'icons/active-19.png',
+      '38': 'icons/active-38.png'
+    },
+    false: manifest.browser_action.default_icon
+  };
+  this.titles = {
+    true: 'Hide number of characters selected',
+    false: manifest.browser_action.default_title
+  };
+
+  // A set of active tab ids (dictionary with key: tabId, value: true)
+  this.activeTabs = {};
+
+  /*
+   * Tab UI event listeners
+   */
+  var self = this;
+
+  // On page loads (and other tab updates), restore active tabs
+  chrome.tabs.onUpdated.addListener(function () {
+    self.get(function (state) {
+      if (state) {
+        self.set(state);
+      }
+    });
+  });
+
+  // When a tab is removed (either by tab close or window close), forget about it
+  chrome.tabs.onRemoved.addListener(function (tabId) {
+    self.set(false, tabId);
+  });
+}
+
+/*
+ * Get the current active tab via single-parameter callback.
+ *
+ * May be undefined.
+ */
+CharacterCount.prototype._getCurrentTab = function (next) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var tabId = tabs[0].id;
+    next(tabId);
+  });
 };
 
-var inactiveTitle = manifest.browser_action.default_title;
-var activeTitle = 'Hide number of characters selected';
+/*
+ * Get the extension state for a tab via single-parameter callback.
+ *
+ * Tab may be specified by trailing optional parameter. If not, current tab
+ * is assumed.
+ */
+CharacterCount.prototype.get = function (next, tabId) {
+  if (!tabId) {
+    var self = this;
+    this._getCurrentTab(function (tabId) {
+      next(tabId in self.activeTabs);
+    });
+  } else {
+    next(tabId in this.activeTabs);
+  }
+};
+
+/*
+ * Set the extension state for a tab.
+ *
+ * Tab may be specified by trailing optional parameter. If not, current tab
+ * is assumed.
+ */
+CharacterCount.prototype.set = function (state, tabId) {
+  var self = this;
+  if (!tabId) {
+    this._getCurrentTab(function (tabId) {
+      if (tabId) {
+        self.set(state, tabId);
+      } // else: no tab specified nor a current tab. Ignore set attempt.
+    });
+  } else {
+    chrome.tabs.get(tabId, function (tab) {
+      if (tab) { // Only modify tab if it exists
+        // Set icon and title
+        chrome.browserAction.setIcon({
+          path: self.icons[state],
+          tabId: tabId
+        });
+        chrome.browserAction.setTitle({
+          title: self.titles[state],
+          tabId: tabId
+        });
+
+        // Message active state to tab content script
+        chrome.tabs.sendMessage(tabId, { active: state });
+      }
+    });
+
+    // Track tab state
+    if (state) {
+      this.activeTabs[tabId] = true;
+    } else {
+      delete this.activeTabs[tabId];
+    }
+  }
+};
+
+/*
+ * Toggle the extension state for a tab.
+ *
+ * Tab may be specified by trailing optional parameter. If not, current tab
+ * is assumed.
+ */
+CharacterCount.prototype.toggle = function (tabId) {
+  var self = this;
+  this.get(function (state) {
+    self.set(!state, tabId);
+  }, tabId);
+};
 
 
-var active = false;
+/*
+ * Initialize extension and listeners
+ */
+var cc = new CharacterCount();
 
+// When the browser action button is clicked, toggle the extension state
+chrome.browserAction.onClicked.addListener(function () {
+  cc.toggle();
+});
 
-function setActiveState(activeState) {
-  active = activeState;
-
-  chrome.browserAction.setIcon({
-    path: active ? activeIcons : inactiveIcons
-  });
-
-  chrome.browserAction.setTitle({
-    title: active ? activeTitle : inactiveTitle
-  });
-
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { active: active });
-  });
-}
-
-function toggleActiveState() {
-  setActiveState(!active);
-}
-
-
-chrome.browserAction.onClicked.addListener(toggleActiveState);
-
+// Listen for messages from the extension to set extension state
 chrome.runtime.onMessage.addListener(function (message) {
-  setActiveState(!!message.active);
+  cc.set(!!message.active);
 });
--- src/content.js
+++ src/content.js
@@ -1,17 +1,21 @@
+/*
+ * Character Count content script
+ */
+
+
+/*
+ * Constants
+ */
 var ESCAPE_KEY = 27;
+var SELECTION_CHANGE_EVENT = 'selectionChange';
 
-var events = [
-  'mousemove',
-  'mouseup',
-  'keydown',
-  'keyup'
-];
 
-var popup;
-
-function createPopup() {
-  var popup = $('<div id="character-count-popup">');
-  popup.css({
+/*
+ * Creates and controls an element that displays character count information
+ */
+function CharacterCountPopup() {
+  this.ID = 'character-count-popup';
+  this.popup = $('<div id="' + this.ID + '">').css({
     'position': 'fixed',
     'z-index': 10000000,
     'top': 0,
@@ -31,72 +35,227 @@ function createPopup() {
     'text-decoration': 'none',
     'vertical-align': 'baseline',
     'user-select': 'none',
-    'pointer-events': 'none',
-    'display': 'none'
-  });
-  return popup;
+    'pointer-events': 'none'
+  })[0];
 }
 
-function showPopup(popup, content) {
-  popup.html(content);
-  popup.css('display', 'block');
-}
+/*
+ * Return message for given count (required)
+ */
+CharacterCountPopup.prototype.getMessageForCount = function (count) {
+  return count + ' character' + (count === 1 ? '' : 's') + ' selected';
+};
 
-function hidePopup(popup) {
-  popup.css('display', 'none');
-}
+/*
+ * Display character count popup with given count (default: 0)
+ */
+CharacterCountPopup.prototype.show = function (count) {
+  var message = this.getMessageForCount(count || 0);
+  $(this.popup).html(message);
+  document.body.appendChild(this.popup);
+};
 
-var prevCharCount;
-var prevText;
-var prevX;
-var prevY;
+/*
+ * Hide character count popup
+ */
+CharacterCountPopup.prototype.hide = function () {
+  var parent = this.popup.parentNode;
+  if (parent) {
+    parent.removeChild(this.popup);
+  }
+};
 
-function showSelectionCharCount(event) {
+
+/*
+ * An object for observing and comparing current text selections.
+ *
+ * When instantiated, captures the current selection and computes a count
+ * of characters selected.
+ *
+ * May be compared for equality against other EquatableSelection instances.
+ *
+ * Should be treated as immutable.
+ */
+function EquatableSelection() {
   var selection = window.getSelection();
-  if (selection) {
-    var text = selection.toString();
-    if (text && text.length) {
-      var charCount = text.length;
-
-      var rect = selection.getRangeAt(0).getBoundingClientRect();
-      var x = rect.left;
-      var y = rect.top;
-
-      if (charCount !== prevCharCount || text !== prevText || x !== prevX || y !== prevY) {
-        var message = charCount + ' character' + (charCount === 1 ? '' : 's') + ' selected';
-        showPopup(popup, message);
-
-        prevCharCount = charCount;
-        prevText = text;
-        prevX = x;
-        prevY = y;
-      }
-    } else {
-      hidePopup(popup);
-    }
-  } else {
-    hidePopup(popup);
+  if (selection && selection.rangeCount === 1) {
+    this.text = selection.toString();
+    this.count = this._computeCount(selection);
   }
 }
 
-var listeningToMouse = false;
-chrome.runtime.onMessage.addListener(function (message) {
-  var shouldAttachListener = !listeningToMouse && message.active;
-  if (shouldAttachListener) {
-    _.each(events, function (event) {
-      $(document.body).on(event, showSelectionCharCount);
-    });
-  } else {
-    _.each(events, function (event) {
-      $(document.body).off(event, showSelectionCharCount);
-    });
+/*
+ * (Private) Compute the number of characters in a EquatableSelection
+ */
+EquatableSelection.prototype._computeCount = function (selection) {
+  var text = selection.toString();
+
+  if (!text || !text.length) {
+    return 0;
   }
-  listeningToMouse = shouldAttachListener;
+
+  var anchorNode = selection.anchorNode; // node containing start of selection
+  var focusNode = selection.focusNode; // node containing end of selection
+
+  // If the anchor node and the focus node are the same, the count of characters
+  // selected is the absolute difference of the anchor and focus offsets
+  if (anchorNode === focusNode) {
+    return Math.abs(selection.focusOffset - selection.anchorOffset);
+  }
+
+  // Consider a groups of newline whitespace to be one space each
+  var countWithCompressedNewlines = text.replace(/[\n\r]+/g, ' ').length;
+
+  // If focus node follows anchor node, selection.toString() sometimes includes
+  // trailing whitespace even if it isn't selected. Decrease count by one if there
+  // is a discrepancy with focusOffset.
+  if (anchorNode.compareDocumentPosition(focusNode) & Node.DOCUMENT_POSITION_FOLLOWING) {
+    if (focusNode.nodeType === Node.TEXT_NODE &&
+        focusNode.data[selection.focusOffset - 1] !== text[text.length - 1]) {
+      return countWithCompressedNewlines - 1;
+    }
+  }
+
+  return countWithCompressedNewlines;
+};
+
+/*
+ * Return true if two EquatableSelection objects are equal, false otherwise
+ */
+EquatableSelection.prototype.isEqual = function (other) {
+  return other &&
+    this.count === other.count &&
+    this.text === other.text;
+};
+
+
+/*
+ * An object that listens for selection changes on a target object and triggers
+ * a custom jQuery 'selectionChange' event when changes are observed.
+ *
+ * Observes changes using EquatableSelection.
+ *
+ * Triggered selectionChange events contain a `selection` property containing an
+ * EquatableSelection if a selection exists. In the case of a previously-selected
+ * selection being unselected, the triggered selectionChange event will not contain
+ * a `selection` property.
+ */
+function SelectionListener(target) {
+  this.target = target;
+
+  // An internal event used to force extra selection change checks
+  this.SELECTION_CHANGE_IMMINENT_EVENT = 'selectionChangeImminent';
+
+  // Events which may indicate selection changes
+  this.events = [
+    'mousemove',
+    'mousedown',
+    'mouseup',
+    'keydown',
+    'keyup',
+    this.SELECTION_CHANGE_IMMINENT_EVENT
+  ];
+
+  this.isListening = false;
+}
+
+/*
+ * Handle events and check for selection changes.
+ *
+ * Trigger selectionChange event if a selection change has occurred.
+ */
+SelectionListener.prototype.handleEvent = function (event) {
+  var current = new EquatableSelection();
+
+  if (current && current.count) {
+    if (!current.isEqual(this.previous)) {
+      // Trigger a selectionChange event if this selection is not equal to
+      // the previously-observed selection
+      $(this.target).trigger({
+        type: SELECTION_CHANGE_EVENT,
+        selection: current
+      });
+      this.previous = current;
+    }
+  } else if (this.previous) {
+    // Trigger empty event to signify that a selection has been "unselected"
+    $(this.target).trigger(SELECTION_CHANGE_EVENT);
+    this.previous = null;
+  }
+
+  // Mouse clicks can cause selections to change, but the selection change
+  // might not have completed yet. Check again shortly.
+  if (event && (event.type === 'mousedown' || event.type === 'mouseup')) {
+    var self = this;
+    setTimeout(function () {
+      $(self.target).trigger(self.SELECTION_CHANGE_IMMINENT_EVENT);
+    }, 20);
+  }
+};
+
+/*
+ * Start listening for selection changes.
+ *
+ * If there is an existing selection when this method is called, a
+ * selectionChange event will be immediately triggered.
+ */
+SelectionListener.prototype.start = function () {
+  if (!this.isListening) {
+    _.each(this.events, function (event) {
+      $(this.target).on(event, this.handleEvent.bind(this));
+    }, this);
+    this.isListening = true;
+
+    // Check for existing seletion immediately
+    $(this.target).trigger(this.SELECTION_CHANGE_IMMINENT_EVENT);
+  }
+};
+
+/*
+ * Stop listening for selection changes.
+ *
+ * One empty selectionChange event is immediately triggered.
+ */
+SelectionListener.prototype.stop = function () {
+  if (this.isListening) {
+    $(this.target).trigger(SELECTION_CHANGE_EVENT);
+    this.previous = null;
+
+    _.each(this.events, function (event) {
+      $(this.target).off(event);
+    }, this);
+    this.isListening = false;
+  }
+};
+
+
+/*
+ * Instantiate popup and and listeners
+ */
+var target = window;
+var popup = new CharacterCountPopup();
+var selectionListener = new SelectionListener(target);
+
+// Listen for messages from other parts of the extension to start/stop selection listening
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.active) {
+    selectionListener.start();
+  } else {
+    selectionListener.stop();
+  }
 });
 
-popup = createPopup();
-document.body.appendChild(popup[0]);
+// Listen for selection changes and show/hide the popup based on the number of
+// characters selected
+$(target).on(SELECTION_CHANGE_EVENT, function (event) {
+  if (event.selection) {
+    popup.show(event.selection.count);
+  } else {
+    popup.hide();
+  }
+});
 
+// When escape key is down, disable the extension
 $(document).on('keydown', function (event) {
   if (event.which === ESCAPE_KEY) {
     chrome.runtime.sendMessage({ active: false });
