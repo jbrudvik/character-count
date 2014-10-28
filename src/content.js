8c200936e55461b6f6f4d65352dba1925c5f3b53
Jeremy Brudvik <jeremy@brudvik.me>
2014-10-27 21:51:22 -0700

    Use selection-counter

--- src/content.js
+++ src/content.js
@@ -1,38 +1,6 @@
 
-/* global chrome:false, $:false, pluralize:false, PeripheryLabel:false, SelectionListener:false */
+/* global SelectionCounter:false */
 
 
-/*
- * Instantiate popup and and listeners
- */
-var COUNTED_NOUN = 'character';
-var popup = new PeripheryLabel(COUNTED_NOUN);
-
-var selectionListener = new SelectionListener(window);
-
-// Listen for selection changes and show/hide the popup based on the number of characters selected
-$(window).on(SelectionListener.SELECTION_CHANGE_EVENT, function (event) {
-  var count = event.selection ? event.selection.characterCount : 0;
-  if (!count) {
-    popup.hide();
-  } else {
-    var message = count + ' ' + pluralize(COUNTED_NOUN, count);
-    popup.show(message);
-  }
-});
-
-// Listen for messages from other parts of the extension to start/stop selection listening
-chrome.runtime.onMessage.addListener(function (message) {
-  if (message.active) {
-    selectionListener.start();
-  } else {
-    selectionListener.stop();
-  }
-});
-
-// On ESC key down, disable the extension
-$(document).on('keydown', function (event) {
-  if (event.which === 27) { // ESC key
-    chrome.runtime.sendMessage({ active: false });
-  }
-});
+var counter = new SelectionCounter('character');
+counter.start();
